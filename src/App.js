288e69d983f43b5080463cc70144f09d71721409
choi jeong ho <113665732+goodcodemakers@users.noreply.github.com>
2023-02-09 17:40:57 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import gpsmap from "./mapjson.json";
-
+import Showbox from "./component/Showbox";
 function App() {
   const time = new Date();
   const year = time.getFullYear(); //년도
@@ -14,7 +14,7 @@ function App() {
   const [humidity, setHumidity] = useState(0);
   const [refsources, setRefsources] = useState(0);
   const [rainfall, serRainfall] = useState(1.0);
-  const [text, setText] = useState("빈값");
+  const [text, setText] = useState("");
   const [wind, setWind] = useState("빈값");
   const [winddirection, setWinddirection] = useState("빈값");
   const [windtext, setWindtext] = useState("북쪽");
@@ -24,12 +24,342 @@ function App() {
   const [hourshumidity, setHourshumidity] = useState([]);
   const [hourswindtext, setHourswindtext] = useState([]);
   const [hourswind, setHourswind] = useState([]);
-  // const [searchtext, setSearchtext] = useState("안양");
+  const [a, seta] = useState([]);
+  const [b, setb] = useState([]);
+  const [show, setShow] = useState([]);
+  const [searchvalue, setSearchvalue] = useState([]);
+
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const selectOne = (e) => {
+    setSelectedOption(e.target.value);
+  };
+  // let selectgu = gpsmap.gps.filter((e) => e.one === "이어도");
+  //종복되는값을 제거하여 뽑아낸뒤 이어도에 일치하는 값을 뽑아내기
+  // let arr2 = [...new Set(selectgu.map((e) => e.two))];
+  //json파일에서 중복되는 값을 제거하여 뽑아내기
+
+  let selectone = [
+    {
+      one: "서울특별시",
+      two: [
+        "종로구",
+        "중구",
+        "용산구",
+        "성동구",
+        "광진구",
+        "동대문구",
+        "중랑구",
+        "성북구",
+        "강북구",
+        "도봉구",
+        "노원구",
+        "은평구",
+        "서대문구",
+        "마포구",
+        "양천구",
+        "강서구",
+        "구로구",
+        "금천구",
+        "영등포구",
+        "동작구",
+        "관악구",
+        "서초구",
+        "강남구",
+        "송파구",
+        "강동구",
+      ],
+    },
+    {
+      one: "부산광역시",
+      two: [
+        "중구",
+        "서구",
+        "동구",
+        "영도구",
+        "부산진구",
+        "동래구",
+        "남구",
+        "북구",
+        "해운대구",
+        "사하구",
+        "금정구",
+        "강서구",
+        "연제구",
+        "수영구",
+        "사상구",
+        "기장군",
+      ],
+    },
+    {
+      one: "대구광역시",
+      two: [
+        "중구",
+        "동구",
+        "서구",
+        "남구",
+        "북구",
+        "수성구",
+        "달서구",
+        "달성군",
+      ],
+    },
+    {
+      one: "인천광역시",
+      two: [
+        "중구",
+        "동구",
+        "미추홀구",
+        "연수구",
+        "남동구",
+        "부평구",
+        "계양구",
+        "서구",
+        "강화군",
+        "옹진군",
+      ],
+    },
+    {
+      one: "광주광역시",
+      two: ["동구", "서구", "남구", "북구", "광산구"],
+    },
+    {
+      one: "대전광역시",
+      two: ["동구", "중구", "서구", "유성구", "대덕구"],
+    },
+    { one: "울산광역시", two: ["중구", "남구", "동구", "북구", "울주군"] },
+    { one: "세종특별자치시", two: ["세종특별자치시"] },
+    {
+      one: "경기도",
+      two: [
+        "수원시장안구",
+        "수원시권선구",
+        "수원시팔달구",
+        "수원시영통구",
+        "성남시수정구",
+        "성남시중원구",
+        "성남시분당구",
+        "의정부시",
+        "안양시만안구",
+        "안양시동안구",
+        "부천시",
+        "광명시",
+        "평택시",
+        "동두천시",
+        "안산시상록구",
+        "안산시단원구",
+        "고양시덕양구",
+        "고양시일산동구",
+        "고양시일산서구",
+        "과천시",
+        "구리시",
+        "남양주시",
+        "오산시",
+        "시흥시",
+        "군포시",
+        "의왕시",
+        "하남시",
+        "용인시처인구",
+        "용인시기흥구",
+        "용인시수지구",
+        "파주시",
+        "이천시",
+        "안성시",
+        "김포시",
+        "화성시",
+        "광주시",
+        "양주시",
+        "포천시",
+        "여주시",
+        "연천군",
+        "가평군",
+        "양평군",
+      ],
+    },
+    {
+      one: "강원도",
+      two: [
+        "춘천시",
+        "원주시",
+        "강릉시",
+        "동해시",
+        "태백시",
+        "속초시",
+        "삼척시",
+        "홍천군",
+        "횡성군",
+        "영월군",
+        "평창군",
+        "정선군",
+        "철원군",
+        "화천군",
+        "양구군",
+        "인제군",
+        "고성군",
+        "양양군",
+      ],
+    },
+    {
+      one: "충청북도",
+      two: [
+        "청주시상당구",
+        "청주시서원구",
+        "청주시흥덕구",
+        "청주시청원구",
+        "충주시",
+        "제천시",
+        "보은군",
+        "옥천군",
+        "영동군",
+        "증평군",
+        "진천군",
+        "괴산군",
+        "음성군",
+        "단양군",
+      ],
+    },
+    {
+      one: "충청남도",
+      two: [
+        "천안시동남구",
+        "천안시서북구",
+        "공주시",
+        "보령시",
+        "아산시",
+        "서산시",
+        "논산시",
+        "계룡시",
+        "당진시",
+        "금산군",
+        "부여군",
+        "서천군",
+        "청양군",
+        "홍성군",
+        "예산군",
+        "태안군",
+      ],
+    },
+    {
+      one: "전라북도",
+      two: [
+        "천안시동남구",
+        "천안시서북구",
+        "공주시",
+        "보령시",
+        "아산시",
+        "서산시",
+        "논산시",
+        "계룡시",
+        "당진시",
+        "금산군",
+        "부여군",
+        "서천군",
+        "청양군",
+        "홍성군",
+        "예산군",
+        "태안군",
+      ],
+    },
+    {
+      one: "전라남도",
+      two: [
+        "목포시",
+        "여수시",
+        "순천시",
+        "나주시",
+        "광양시",
+        "담양군",
+        "곡성군",
+        "구례군",
+        "고흥군",
+        "보성군",
+        "화순군",
+        "장흥군",
+        "강진군",
+        "해남군",
+        "영암군",
+        "무안군",
+        "함평군",
+        "영광군",
+        "장성군",
+        "완도군",
+        "진도군",
+        "신안군",
+      ],
+    },
+    {
+      one: "경상북도",
+      two: [
+        "포항시남구",
+        "포항시북구",
+        "경주시",
+        "김천시",
+        "안동시",
+        "구미시",
+        "영주시",
+        "영천시",
+        "상주시",
+        "문경시",
+        "경산시",
+        "군위군",
+        "의성군",
+        "청송군",
+        "영양군",
+        "영덕군",
+        "청도군",
+        "고령군",
+        "성주군",
+        "칠곡군",
+        "예천군",
+        "봉화군",
+        "울진군",
+        "울릉군",
+      ],
+    },
+    {
+      one: "경상남도",
+      two: [
+        "창원시의창구",
+        "창원시성산구",
+        "창원시마산합포구",
+        "창원시마산회원구",
+        "창원시진해구",
+        "진주시",
+        "통영시",
+        "사천시",
+        "김해시",
+        "밀양시",
+        "거제시",
+        "양산시",
+        "의령군",
+        "함안군",
+        "창녕군",
+        "고성군",
+        "남해군",
+        "하동군",
+        "산청군",
+        "함양군",
+        "거창군",
+        "합천군",
+      ],
+    },
+    { one: "제주특별자치도", two: ["제주시", "서귀포시"] },
+    {
+      one: "이어도",
+    },
+  ];
+  console.log(selectone[0].two[0]);
   // const [filterdata, setFilterdata] = useState([]);
+  // let btnSequence = [
+  //   "현재",
+  //   "1시간뒤",
+  //   "2시간뒤",
+  //   "3시간뒤",
+  //   "4시간뒤",
+  //   "5시간뒤",
+  //   "6시간뒤",
+  // ];
 
-  // setFilterdata(filteringdata);
-
-  console.log(gpsmap.gps[3].two);
   const onehours = "시간";
   if (month < 10) {
     //월이 10월보다 작을경우 01로 나오게 설정
@@ -69,7 +399,9 @@ function App() {
     fetch(url_API)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data.response.body.items.item);
+
+        // seta([text, ddddd]);
         // const filteringdata = gpsmap.gps.filter((item) =>
         //   // item.two.includes(searchtext)
         // );
@@ -100,20 +432,19 @@ function App() {
           let futureRainfall = hoursrainfall;
           futureRainfall.push(data.response.body.items.item[i].fcstValue);
           if (futureRainfall[i - 12] == "강수없음") {
-            futureRainfall[i - 12] = "강수량이 없습니다";
+            futureRainfall[i - 12] = 0;
           } else if (futureRainfall[i - 12] == "1.0mm 미만") {
-            futureRainfall[i - 12] = "1.0mm 미만의 강수량이 있습니다";
+            futureRainfall[i - 12] = "1.0mm 미만";
           } else if (futureRainfall[i - 12] == "30.0~50.0mm") {
-            futureRainfall[i - 12] = "30.0~50.0mm의 강수량이 있습니다";
+            futureRainfall[i - 12] = "30.0~50.0mm";
           } else if (futureRainfall[i - 12] == "50.0mm 이상") {
-            futureRainfall[i - 12] = "50.0mm 이상 강수량이 있습니다";
+            futureRainfall[i - 12] = "50.0mm 이상";
           } else {
-            futureRainfall[i - 12] = `${
-              futureRainfall[i - 12]
-            }의 강수량이 있습니다`;
+            futureRainfall[i - 12] = `${futureRainfall[i - 12]}`;
           }
           setHoursrainfall([...futureRainfall]);
         }
+
         //18~23 하늘형태 "3"맑음
         //24~29 기온 "8"도
         for (let i = 24; i <= 29; i++) {
@@ -150,6 +481,69 @@ function App() {
           }
           setHourswindtext([...futureWindtext]);
         }
+        seta([
+          {
+            id: 1,
+            active: false,
+            weather: weather[0],
+            temper: hourstemperature[0],
+            humidity: hourshumidity[0],
+            rain: hoursrainfall[0],
+            wind: hourswind[0],
+            windtext: hourswindtext[0],
+          },
+          {
+            id: 2,
+            active: false,
+            weather: weather[1],
+            temper: hourstemperature[1],
+            humidity: hourshumidity[1],
+            rain: hoursrainfall[1],
+            wind: hourswind[1],
+            windtext: hourswindtext[1],
+          },
+          {
+            id: 3,
+            active: false,
+            weather: weather[2],
+            temper: hourstemperature[2],
+            humidity: hourshumidity[2],
+            rain: hoursrainfall[2],
+            wind: hourswind[2],
+            windtext: hourswindtext[2],
+          },
+          {
+            id: 4,
+            active: false,
+            weather: weather[3],
+            temper: hourstemperature[3],
+            humidity: hourshumidity[3],
+            rain: hoursrainfall[3],
+            wind: hourswind[3],
+            windtext: hourswindtext[3],
+          },
+          {
+            id: 5,
+            active: false,
+            weather: weather[4],
+            temper: hourstemperature[4],
+            humidity: hourshumidity[4],
+            rain: hoursrainfall[4],
+            wind: hourswind[4],
+            windtext: hourswindtext[4],
+          },
+          {
+            id: 6,
+            active: false,
+            weather: weather[5],
+            temper: hourstemperature[5],
+            humidity: hourshumidity[5],
+            rain: hoursrainfall[5],
+            wind: hourswind[5],
+            windtext: hourswindtext[5],
+          },
+        ]);
+
         //54~59 풍속  "2"m/s
         for (let i = 54; i <= 59; i++) {
           let futurewind = hourswind;
@@ -197,143 +591,86 @@ function App() {
         }
       });
   }, []);
-  // const changetext = (event) => {
-  //   setSearchtext(event.target.value);
-  // };
+
+  useEffect(() => {
+    setb([
+      {
+        id: 0,
+        active: true,
+        weather: text,
+        temper: temperature,
+        humidity: humidity,
+        rain: rainfall,
+        wind: wind,
+        windtext: windtext,
+      },
+      ...a,
+    ]);
+  }, [text, temperature, humidity, rainfall, wind, windtext, a]);
+
+  const toggle = (key) => {
+    setb((b) =>
+      b.map((e) =>
+        e.id == key ? { ...e, active: true } : { ...e, active: false }
+      )
+    );
+  };
+
+  useEffect(() => {
+    let show1 = b.filter((e) => e.active == true);
+    setShow(show1);
+  }, [b]);
+
   return (
     <div className="container">
       <h1 className="title">일기 단기 예보</h1>
       <div className="currently">
         <div className="inputlist">
-          <input type="text" placeholder="시/군/구를 입력하세요" />
+          <select className="selectStyle" onChange={selectOne}>
+            {selectone.map((e, i) => (
+              <option value={e.one} key={i}>
+                {e.one}
+              </option>
+            ))}
+          </select>
+          <select
+            className="selectStyle"
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+          >
+            {selectone.map(
+              (e, i) =>
+                e.one === selectedOption &&
+                e.two.map((t, j) => (
+                  <option value={e.two[t]} key={j}>
+                    {t}
+                  </option>
+                ))
+            )}
+          </select>
         </div>
-        <div>
-          <div className="col">
-            <h3>현재 날씨</h3>
-            <p>{text}</p>
-            <h3>현재 기온</h3>
-            <p>{temperature}º 입니다.</p>
-
-            <h3>습도</h3>
-            <p>{humidity}%입니다.</p>
-            <h3>
-              1시간 <br />
-              강수량
-            </h3>
-            <p>
-              {rainfall == 0
-                ? "강수량이 없습니다."
-                : `현재 강수량은 ${rainfall}mm미만입니다.`}
-            </p>
-            {/*RN1 2배열 */}
-
-            <h3>풍속</h3>
-            <p>{wind}m/s 입니다</p>
-            {/* WSD 7배열 */}
-            <h3>풍향</h3>
-            <p>현재 {windtext} 입니다.</p>
-            {/* VEC 5배열 */}
-          </div>
+        <div className="present">
+          <h3>현재 날씨</h3>
+          <Showbox show={show} />
         </div>
       </div>
       <div className="weatherbtn">
         <div className="colbtn">
-          <button className="btnsize">현재</button>
-          <button className="btnsize">1시간후</button>
-          <button className="btnsize">2시간후</button>
+          {b.map((e) => (
+            <>
+              <button
+                className="btnsize"
+                key={e.id}
+                onClick={() => {
+                  toggle(e.id);
+                }}
+              >
+                {e.id === 0 ? "현재" : e.id + "시간"}
+              </button>
+            </>
+          ))}
         </div>
-        <div className="colbtn">
-          <button className="btnsize">3시간후</button>
-          <button className="btnsize">4시간후</button>
-          <button className="btnsize">5시간후</button>
-          <button className="btnsize">6시간후</button>
-        </div>
-      </div>
-      <div className="onehours">
-        <h3>1{onehours} 후 날씨</h3>
-        <p>
-          1{onehours} 뒤 {weather[0]}
-        </p>
-        <h3> 기온</h3>
-        <p>{hourstemperature[0]}º 입니다.</p>
-        <h3>습도</h3>
-        <p>{hourshumidity[0]}%입니다.</p>
-        <h3>1{onehours} 강수량</h3>
-        <p>{hoursrainfall[0]}</p>
-        <h3>풍속</h3>
-        <p>{hourswind[0]}m/s 입니다</p>
-        <h3>풍향</h3>
-        <p> {hourswindtext[0]} 입니다.</p>
-      </div>
-      <div className="twohours">
-        <h3>두 시간 후 날씨</h3>
-        <p>두 시간 뒤 {weather[1]}</p>
-        <h3> 기온</h3>
-        <p>{hourstemperature[1]}º 입니다.</p>
-        <h3>습도</h3>
-        <p>{hourshumidity[1]}%입니다.</p>
-        <h3>1시간 강수량</h3>
-        <p>{hoursrainfall[1]}</p>
-        <h3>풍속</h3>
-        <p>{hourswind[1]}m/s 입니다</p>
-        <h3>풍향</h3>
-        <p> {hourswindtext[1]} 입니다.</p>
-      </div>
-      <div className="threehours">
-        <h3>세 시간 후 날씨</h3>
-        <p>세 시간 뒤 {weather[2]}</p>
-        <h3> 기온</h3>
-        <p>{hourstemperature[2]}º 입니다.</p>
-        <h3>습도</h3>
-        <p>{hourshumidity[2]}%입니다.</p>
-        <h3>1시간 강수량</h3>
-        <p>{hoursrainfall[2]}</p>
-        <h3>풍속</h3>
-        <p>{hourswind[2]}m/s 입니다</p>
-        <h3>풍향</h3>
-        <p> {hourswindtext[2]} 입니다.</p>
-      </div>
-      <div className="fourhours">
-        <h3>네 시간 후 날씨</h3>
-        <p>네 시간 뒤 {weather[3]}</p>
-        <h3> 기온</h3>
-        <p>{hourstemperature[3]}º 입니다.</p>
-        <h3>습도</h3>
-        <p>{hourshumidity[3]}%입니다.</p>
-        <h3>1시간 강수량</h3>
-        <p>{hoursrainfall[3]}</p>
-        <h3>풍속</h3>
-        <p>{hourswind[3]}m/s 입니다</p>
-        <h3>풍향</h3>
-        <p> {hourswindtext[3]} 입니다.</p>
-      </div>
-      <div className="fivehours">
-        <h3>다섯 시간 후 날씨</h3>
-        <p>다섯 시간 뒤 {weather[4]}</p>
-        <h3> 기온</h3>
-        <p>{hourstemperature[4]}º 입니다.</p>
-        <h3>습도</h3>
-        <p>{hourshumidity[4]}%입니다.</p>
-        <h3>1시간 강수량</h3>
-        <p>{hoursrainfall[4]}</p>
-        <h3>풍속</h3>
-        <p>{hourswind[4]}m/s 입니다</p>
-        <h3>풍향</h3>
-        <p> {hourswindtext[4]} 입니다.</p>
-      </div>
-      <div className="sixehours">
-        <h3>여섯 시간 후 날씨</h3>
-        <p>여섯 시간 뒤 {weather[5]}</p>
-        <h3> 기온</h3>
-        <p>{hourstemperature[5]}º 입니다.</p>
-        <h3>습도</h3>
-        <p>{hourshumidity[5]}%입니다.</p>
-        <h3>1시간 강수량</h3>
-        <p>{hoursrainfall[5]}</p>
-        <h3>풍속</h3>
-        <p>{hourswind[5]}m/s 입니다</p>
-        <h3>풍향</h3>
-        <p> {hourswindtext[5]} 입니다.</p>
       </div>
     </div>
   );
---
+++ src/component/Showbox.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Showbox({ show }) {
+  return (
+    <>
+      {show.length !== 0 ? (
+        <div className="col">
+          <p>{show[0].weather}</p>
+          <h3>현재 기온</h3>
+          <p>{show[0].temper}º 입니다.</p>
+
+          <h3>습도</h3>
+          <p>{show[0].humidity}%입니다.</p>
+          <h3>
+            1시간 <br />
+            강수량
+          </h3>
+          <p>
+            {show[0].rain == 0
+              ? "강수량이 없습니다."
+              : `{show[0].id === 0 ? "현재" : show[0].id + "시간뒤의"} 강수량은 ${show[0].rain}입니다.`}
+          </p>
+
+          <h3>풍속</h3>
+          <p>{show[0].wind}m/s 입니다</p>
+
+          <h3>풍향</h3>
+          <p>현재 {show[0].windtext} 입니다.</p>
+        </div>
+      ) : (
+        <p>"로딩중"</p>
+      )}
+    </>
+  );
+}
